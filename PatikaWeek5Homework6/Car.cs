fb8cf3356d7b6961777a6396f49d17c5edb62eed
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-19 09:53:25 +0300

    Add project files.

---
+++ PatikaWeek5Homework6/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek5Homework6
+{
+    internal class Car
+    {
+        //Araba nesnesinin property'leri
+        public DateTime ProductionDate { get; }
+        public string SerialNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public int NumberOfDoors { get; set; }
+
+        public Car()
+        {
+            ProductionDate = DateTime.Now;//Araba nesnesi üretildiğinde üretim tarihini atar
+        }
+
+        public void CarInfo()
+        {
+           
+            Console.WriteLine($"\nSeri Numarası..:{SerialNumber} \nMarka..:{Brand}");
+        }
+
+    }
+}
+
---
+++ PatikaWeek5Homework6/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using PatikaWeek5Homework6;
+
+
+        List<Car> cars = new List<Car>();//Car tipinde liste tanımlanıyor.
+
+        while (true)
+        {
+        carsGo: Console.WriteLine("\nYeni bir araba eklemek ister misiniz?(e/h)");
+            var continueFilm = Console.ReadLine().ToLower();
+            if (continueFilm == "h")
+            {
+                break;
+            }
+            else if (continueFilm != "e")
+            {
+             
+                Console.WriteLine("Geçersiz giriş.");
+                goto carsGo;
+            }
+
+          cars.Add(CarProduction());//CarProduction metodundan yeni araba bilgileri alınıyor ve listeye ekleniyor
+
+        }
+
+        Console.WriteLine("\nÜretilen araba listesi.....");
+        foreach (var car in cars)
+        {
+            car.CarInfo();
+        }
+
+
+        ///<summary>
+        ///Araba nesnesinin üretildiği metot.
+        ///</summary>
+        Car CarProduction()
+        {
+            Car car = new Car();
+            Console.WriteLine("\nAraba seri numarasını giriniz..: ");
+            car.SerialNumber = Console.ReadLine();
+            Console.WriteLine("\nAraba markasını giriniz..: ");
+            car.Brand = Console.ReadLine();
+            Console.WriteLine("Araba modelini giriniz..:");
+            car.Model = Console.ReadLine();
+            Console.WriteLine("Araba rengini giriniz..:");
+            car.Color = Console.ReadLine();
+
+            Console.WriteLine("Arabanın kapı sayısını giriniz..:");
+        doorsGo:
+            try
+            {
+                car.NumberOfDoors = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen kapı sayısını giriniz.");
+                goto doorsGo;
+            }
+
+            return car;
+        }
+    
